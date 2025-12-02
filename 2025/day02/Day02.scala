9341c33b3ee47cd9b6da0f1d87eb0dcceeebf100
Bhavya Muni <bhavya.muni@rootly.com>
2025-12-02 12:36:30 -0500

    feat: day 2 p1

---
+++ 2025/day02/Day02.scala
@@ -0,0 +1,44 @@
+import scala.io.Source
+
+object Day02 {
+  def part1(input: String): BigInt = {
+    val lines = input.split("\n")
+    val ranges = lines.head.split(",")
+    val out =
+      ranges.foldLeft(BigInt(0))((acc, curr) =>
+        acc + iterateAndCheck(getMinAndMax(curr)._1, getMinAndMax(curr)._2).sum
+      )
+    out
+  }
+
+  def getMinAndMax(input: String): (BigInt, BigInt) =
+    (BigInt(input.split("-")(0)), BigInt(input.split("-")(1)))
+
+  def isRepeated(input: String): Boolean = {
+    input match
+      case ""                                                 => true
+      case _ if input.length % 2 == 1                         => false
+      case _ if input.length == 2 && input.head == input.last => true
+      case _                                                  =>
+        isRepeated(
+          input.tail.take(input.tail.length / 2) +
+            input.tail.drop(input.tail.length / 2 + 1)
+        ) && input.head == input(input.length / 2)
+  }
+
+  def iterateAndCheck(min: BigInt, max: BigInt): Seq[BigInt] = {
+    (min to max).filter((a) => isRepeated(a.toString))
+  }
+
+  def part2(input: String): Int = {
+    val lines = input.split("\n")
+    0
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = Source.fromFile("2025/day02/input.txt").mkString
+
+    println(s"Part 1: ${part1(input)}")
+    println(s"Part 2: ${part2(input)}")
+  }
+}
