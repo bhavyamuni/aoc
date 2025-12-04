a2c6125b1d2292e04a0c48f6ccd55a4d98006f93
Bhavya Muni <bhavyamuni@outlook.com>
2025-12-04 15:08:04 -0500

    chore: change day 3 to functional

--- 2025/day03/Day03.scala
+++ 2025/day03/Day03.scala
@@ -19,34 +19,56 @@ object Day03 {
       ._2
   }
 
-  def checkMax(possibilities: String): BigInt = {
-    var out = Set[BigInt]()
-    var st = Stack(0)
-
-    for (ix <- 0 to (possibilities.length - 1)) {
-      val i = possibilities(ix).asDigit
-      while (
-        st.length > 0 && st.top < i && (st.length + possibilities.length - ix) > 12
-      ) {
-        st.pop()
-      }
-      st.push(i)
-      if (st.length == 12) {
-        out += BigInt(st.map(_.toString).reduceRight((a, b) => b + a))
-      }
+  def checkMaxRecurs(
+      possibilities: String,
+      st: Stack[Int]
+  ): BigInt = {
+    st.length match {
+      case 12 => BigInt(st.map(_.toString).reduceRight((a, b) => b + a))
+      case _ if possibilities.length <= 0 => BigInt(0)
+      case _
+          if st.length > 0 && st.top < possibilities.head.asDigit && (st.length + possibilities.length) > 12 =>
+        checkMaxRecurs(
+          possibilities,
+          st.tail
+        )
+      case _ =>
+        checkMaxRecurs(
+          possibilities.tail,
+          st.push(possibilities.head.asDigit)
+        )
     }
-    out.max
+  }
+
+  // imperative
+  def checkMax(possibilities: String): BigInt = {
+    var st = Stack(0)
+    possibilities.zipWithIndex
+      .foldLeft(BigInt(0))((acc, state) => {
+        val (x, idx) = state
+        while (
+          st.length > 0 && st.top < x.asDigit && (st.length + possibilities.length - idx) > 12
+        ) {
+          st.pop()
+        }
+        st.push(x.asDigit)
+        if st.length == 12 then
+          acc.max(BigInt(st.map(_.toString).reduceRight((a, b) => b + a)))
+        else acc
+      })
   }
 
   def part2(input: String): BigInt = {
     val lines = input.split("\n")
-    lines.foldLeft(BigInt(0))((a, b) => a + checkMax(b))
+    lines.foldLeft(BigInt(0))((a, b) => a + checkMaxRecurs(b, Stack(0)))
   }
 
   def main(args: Array[String]): Unit = {
+    val startTime = System.nanoTime()
     val input = Source.fromFile("2025/day03/input.txt").mkString
 
     println(s"Part 1: ${part1(input)}")
     println(s"Part 2: ${part2(input)}")
+    println(s"Run time: ${(System.nanoTime() - startTime) / 1000} microseconds")
   }
 }
--- 2025/template.scala
+++ 2025/template.scala
@@ -12,9 +12,11 @@ object DayXX {
   }
 
   def main(args: Array[String]): Unit = {
+    val startTime = System.nanoTime()
     val input = Source.fromFile("2025/dayXX/input.txt").mkString
 
     println(s"Part 1: ${part1(input)}")
     println(s"Part 2: ${part2(input)}")
+    println(s"Run time: ${(System.nanoTime() - startTime) / 1000} microseconds")
   }
 }
